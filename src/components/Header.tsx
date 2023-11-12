a4fe544e023a1087ad1024e104c3bd6a27c053aa
J3romey <jerome.adviento@hotmail.com>
2023-11-12 13:24:57 -0600

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,11 @@ import {Link} from "react-router-dom";
 
 function Header() {
     return (
-        <div className="sticky">
-            <Link to={`/games`}>Aerobox Games</Link>
+        <div className="sticky top-0 py-4 px-8 bg-opacity-80 bg-black w-full">
+            <button className="bg-black font-bold py-4 px-8 rounded
+                                hover:bg-white hover:text-black ">
+                <Link to={`/games`}>Aerobox Games</Link>
+            </button>
         </div>
     )
 }
