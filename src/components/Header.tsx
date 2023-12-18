5ab8d969848d0ad9481c5cdf56278ca601a02a48
J3romey <jerome.adviento@hotmail.com>
2023-12-18 01:39:06 -0600

    remove game prefix as its not working

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
         <div className="sticky z-10 top-0 py-4 px-8 bg-opacity-80 bg-black w-full">
             <button className="bg-black font-bold py-4 px-8 rounded
                                 hover:bg-white hover:text-black ">
-                <a href="/games/">
+                <a href="/">
                     Aerobox Games
                 </a>
             </button>
