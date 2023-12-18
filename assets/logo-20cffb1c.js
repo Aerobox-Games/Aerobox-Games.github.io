77739bb6f842cd704a4c7a284f465c73331c926a
j3romey <j3romey@users.noreply.github.com>
2023-12-18 08:30:21 +0000

    deploy: 17af520ec2c54bb5a4a2c22f048dd623303dfa59

---
+++ assets/logo-20cffb1c.js
@@ -0,0 +1,1 @@
+const a="/assets/parallax-2a5aae36.webp",s="/assets/logo-6054d95b.png";export{s as L,a as P};
--- assets/logo-21d33a50.js
+++
@@ -1,1 +0,0 @@
-const a="/assets/parallax-fffe3807.png",s="/assets/logo-6054d95b.png";export{s as L,a as P};
