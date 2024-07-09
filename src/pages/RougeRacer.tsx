bce13f3bb0544ffae33ea5d8eb0e4420be9a1190
J3romey <jerome.adviento@hotmail.com>
2024-07-09 13:51:06 -0600

    update logo header

--- src/pages/RougeRacer.tsx
+++ src/pages/RougeRacer.tsx
@@ -1,11 +1,12 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 
-import RogueRacerLogo from "../assets/rogue-racer/logo.png"
+import RogueRacerLogo from "../assets/rogue-racer/logo2.png"
+import RogueRacerLogoBackground from "../assets/rogue-racer/backgrounds/logo-background.png"
 // import RogueRacerMask from "../assets/rogue-racer/mask.png"
 
-import CyberRacerImage from "../assets/rogue-racer/cyber_racer.webp"
-import GyaruRacerImage from "../assets/rogue-racer/gyaru_racer.webp"
+//import CyberRacerImage from "../assets/rogue-racer/cyber_racer.webp"
+//import GyaruRacerImage from "../assets/rogue-racer/gyaru_racer.webp"
 
 import YouTubeEmbed from "../components/YouTubeEmbed"
 import ScreenshotCarousel from "../components/rouge-racer/ScreenshotCarousel"
@@ -19,58 +20,60 @@ function RougeRacer() {
     return (
         <div className=" bg-black">
             <Header/>
+            <div className="flex justify-center relative overflow-hidden bg-cover bg-no-repeat p-16" style={{backgroundImage: `url(${RogueRacerLogoBackground})`}}  >
+                        <img className="w-4/6
+                                        lg:w-2/5" 
+                            src={RogueRacerLogo}/>
 
-            <div className="flex justify-center relative overflow-hidden">
-                    <img className="w-3/6
+                    {/* <img className="absolute top-[0%] h-[200%] left-[5%]
+                                    md:left-[20%]" 
+                        src={GyaruRacerImage}/> */}
+                    
+                    {/* <img className="absolute  top-[5%] h-[200%] right-[8%]
+                                    md:right-[21%]" 
+                        src={CyberRacerImage}/> */}
+                </div>
+            <div className="max-w-screen-lg mx-auto">
+ 
+
+                <div className="mx-4 mb-4 py-8
+                                md:mx-[10%] md:py-24 md:mb-12
+                                lg:mx-[15%]">
+                    <YouTubeEmbed embedId="foKgcQQnMok"/>
+                </div>
+                
+                {/* <div className="relative py-16 bg-w">
+                    <div className="relative">
+                        <GlitchImages />
+
+                        <img className="absolute top-0 w-full" src={RogueRacerMask}/>
+                    </div>
+
+                    <img className="absolute top-[15%] w-3/6 left-2/4 translate-y-[-50%] translate-x-[-50%]
                                     lg:w-2/6" 
                         src={RogueRacerLogo}/>
 
-                <img className="absolute top-[0%] h-[200%] left-[5%]
-                                md:left-[20%]" 
-                    src={GyaruRacerImage}/>
-                
-                <img className="absolute  top-[5%] h-[200%] right-[8%]
-                                md:right-[21%]" 
-                    src={CyberRacerImage}/>
-            </div>
 
-            <div className="mx-4 mb-4 py-8
-                            md:mx-[10%] md:py-24 md:mb-12
-                            lg:mx-[15%]">
-                <YouTubeEmbed embedId="foKgcQQnMok"/>
-            </div>
-            
-            {/* <div className="relative py-16 bg-w">
-                <div className="relative">
-                    <GlitchImages />
+                </div> */}
 
-                    <img className="absolute top-0 w-full" src={RogueRacerMask}/>
+                {/* <div className="mx-4 py-8 text-center">
+                    <ContentHeader text="About"/> 
+                    <p>Rogue Racer is an upcoming racing game with narrative and combat elements developed by Aerobox Games.</p>
+                    <p>Stay tuned! </p>
+                </div> */}
+
+                <div className="px-2 mb-4 flex justify-center 
+                                md:mx-[10%] md:py-24 md:mb-12
+                                lg:mx-[5%]">
+                    <iframe src="https://store.steampowered.com/widget/764420/" frameBorder="0" width="646" height="190"></iframe>
                 </div>
 
-                <img className="absolute top-[15%] w-3/6 left-2/4 translate-y-[-50%] translate-x-[-50%]
-                                lg:w-2/6" 
-                    src={RogueRacerLogo}/>
-
-
-            </div> */}
-
-            {/* <div className="mx-4 py-8 text-center">
-                <ContentHeader text="About"/> 
-                <p>Rogue Racer is an upcoming racing game with narrative and combat elements developed by Aerobox Games.</p>
-                <p>Stay tuned! </p>
-            </div> */}
-
-            <div className="px-2 mb-4 flex justify-center 
-                            md:mx-[10%] md:py-24 md:mb-12
-                            lg:mx-[5%]">
-                <iframe src="https://store.steampowered.com/widget/764420/" frameBorder="0" width="646" height="190"></iframe>
-            </div>
-
-            <div className="mx-4 py-8
-                            md:px-[5%]
-                            lg:px-[2%]">
-                <ContentHeader text="Screenshots"/>                
-                <ScreenshotCarousel />
+                <div className="mx-4 py-8
+                                md:px-[5%]
+                                lg:px-[2%]">
+                    <ContentHeader text="Screenshots"/>                
+                    <ScreenshotCarousel />
+                </div>
             </div>
 
             <Footer/>
